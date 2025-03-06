421803985956d4ab9bffa9dd966f488347859d06
jguirim-seif <114693084+jguirim-seif@users.noreply.github.com>
2025-03-06 13:39:55 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game');
 
 var mainState = {
-
     preload: function() { 
         game.stage.backgroundColor = '#71c5cf';
-        bird = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEXSvicAAABogyUZAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
-        pipe = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEV0vy4AAADnrrHQAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
-        game.load.image('bird', bird);  
-        game.load.image('pipe', pipe); 
+
+        // Base64 images
+        var birdBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEXSvicAAABogyUZAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
+        var pipeBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEV0vy4AAADnrrHQAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
+
+        // Add base64 images to Phaser's cache
+        game.load.onLoadComplete.addOnce(() => {
+            game.cache.addBase64('bird', birdBase64);
+            game.cache.addBase64('pipe', pipeBase64);
+        });
+
+        // Dummy load to trigger `onLoadComplete`
+        game.load.image('dummy', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAA...");
     },
 
     create: function() { 
@@ -22,54 +30,38 @@ var mainState = {
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000; 
 
-        // New anchor position
         this.bird.anchor.setTo(-0.2, 0.5); 
- 
+
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this); 
 
         this.score = 0;
         this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
 
-        // Add the jump sound
-        this.jumpSound = this.game.add.audio('jump');
+        this.jumpSound = game.add.audio('jump');
     },
 
     update: function() {
-        if (this.bird.inWorld == false)
-            this.restartGame(); 
+        if (!this.bird.inWorld) this.restartGame(); 
 
         game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this); 
 
-        // Rotate the bird 
-        if (this.bird.angle < 20)
-            this.bird.angle += 1;   
+        if (this.bird.angle < 20) this.bird.angle += 1;   
     },
 
     jump: function() {
-        // If the bird is dead, he can't jump
-        if (this.bird.alive == false)
-            return; 
+        if (!this.bird.alive) return; 
 
         this.bird.body.velocity.y = -350;
-
-        // Jump animation
         game.add.tween(this.bird).to({angle: -20}, 100).start();
-
     },
 
     hitPipe: function() {
-        // If the bird has already hit a pipe, we have nothing to do
-        if (this.bird.alive == false)
-            return;
-            
-        // Set the alive property of the bird to false
-        this.bird.alive = false;
+        if (!this.bird.alive) return;
 
-        // Prevent new pipes from appearing
+        this.bird.alive = false;
         this.game.time.events.remove(this.timer);
-    
-        // Go through all the pipes, and stop their movement
+
         this.pipes.forEachAlive(function(p){
             p.body.velocity.x = 0;
         }, this);
@@ -81,7 +73,6 @@ var mainState = {
 
     addOnePipe: function(x, y) {
         var pipe = this.pipes.getFirstDead();
-
         pipe.reset(x, y);
         pipe.body.velocity.x = -200;  
         pipe.checkWorldBounds = true;
@@ -89,16 +80,18 @@ var mainState = {
     },
 
     addRowOfPipes: function() {
-        var hole = Math.floor(Math.random()*5)+1;
+        var hole = Math.floor(Math.random() * 5) + 1;
         
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole +1) 
-                this.addOnePipe(400, i*60+10);   
-    
+        for (var i = 0; i < 8; i++) {
+            if (i !== hole && i !== hole + 1) {
+                this.addOnePipe(400, i * 60 + 10);
+            }
+        }
+
         this.score += 1;
-        this.labelScore.text = this.score;  
+        this.labelScore.text = this.score;
     },
 };
 
 game.state.add('main', mainState);  
-game.state.start('main'); 
+game.state.start('main');
