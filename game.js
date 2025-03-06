2d704debd96459be0d47103e12e234704b5929ac
jguirim-seif <114693084+jguirim-seif@users.noreply.github.com>
2025-03-06 13:57:21 +0100

    Update game.js

--- game.js
+++ game.js
@@ -4,17 +4,8 @@ var mainState = {
     preload: function() { 
         game.stage.backgroundColor = '#71c5cf';
 
-        var birdImg = new Image();
-        birdImg.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEXSvicAAABogyUZAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
-        birdImg.onload = function () {
-            game.cache.addImage('bird', birdImg.src, birdImg);
-        };
-
-        var pipeImg = new Image();
-        pipeImg.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEV0vy4AAADnrrHQAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==";
-        pipeImg.onload = function () {
-            game.cache.addImage('pipe', pipeImg.src, pipeImg);
-        };
+        game.load.image('bird', "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEXSvicAAABogyUZAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==");
+        game.load.image('pipe', "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEV0vy4AAADnrrHQAAAAGUlEQVR4AWP4DwYHMOgHDEDASCN6lMYV7gChf3AJ/eB/pQAAAABJRU5ErkJggg==");
     },
 
     create: function() { 
@@ -23,36 +14,35 @@ var mainState = {
         this.pipes = game.add.group();
         this.pipes.enableBody = true;
         this.pipes.createMultiple(20, 'pipe');  
-        this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);           
 
-        this.bird = this.game.add.sprite(game.world.centerX - 50, game.world.centerY, 'bird');
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+        this.bird = game.add.sprite(100, game.world.centerY, 'bird');
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000; 
 
         this.bird.anchor.setTo(-0.2, 0.5); 
 
-        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this); 
 
         this.score = 0;
-        this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
 
-        this.finalScoreText = this.game.add.text(
-            game.world.centerX, game.world.centerY, "", 
-            { font: "50px Arial", fill: "#ff0000", align: "center" }
-        );
+        this.finalScoreText = game.add.text(game.world.centerX, game.world.centerY, "", 
+            { font: "50px Arial", fill: "#ff0000", align: "center" });
         this.finalScoreText.anchor.setTo(0.5, 0.5);
         this.finalScoreText.visible = false;
 
-        this.jumpSound = game.add.audio('jump');
+        this.bird.alive = true; // Fix: Ensure the bird starts alive
     },
 
     update: function() {
-        if (!this.bird.inWorld) this.endGame(); 
-
-        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this); 
-
-        if (this.bird.angle < 20) this.bird.angle += 1;   
+        if (this.bird.alive) {
+            if (!this.bird.inWorld) this.endGame(); 
+            game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+            if (this.bird.angle < 20) this.bird.angle += 1;   
+        }
     },
 
     jump: function() {
@@ -68,7 +58,7 @@ var mainState = {
         this.bird.alive = false;
         this.game.time.events.remove(this.timer);
 
-        this.pipes.forEachAlive(function(p){
+        this.pipes.forEachAlive(function(p) {
             p.body.velocity.x = 0;
         }, this);
 
@@ -76,6 +66,9 @@ var mainState = {
     },
 
     endGame: function() {
+        if (!this.bird.alive) return; 
+
+        this.bird.alive = false;
         this.finalScoreText.text = "Game Over\nScore: " + this.score;
         this.finalScoreText.visible = true;
 
@@ -86,15 +79,17 @@ var mainState = {
 
     addOnePipe: function(x, y) {
         var pipe = this.pipes.getFirstDead();
-        pipe.reset(x, y);
-        pipe.body.velocity.x = -200;  
-        pipe.checkWorldBounds = true;
-        pipe.outOfBoundsKill = true;
+        if (pipe) {
+            pipe.reset(x, y);
+            pipe.body.velocity.x = -200;  
+            pipe.checkWorldBounds = true;
+            pipe.outOfBoundsKill = true;
+        }
     },
 
     addRowOfPipes: function() {
         var hole = Math.floor(Math.random() * 5) + 1;
-        
+
         for (var i = 0; i < 8; i++) {
             if (i !== hole && i !== hole + 1) {
                 this.addOnePipe(game.world.width, i * 60 + 10);
@@ -109,7 +104,6 @@ var mainState = {
 game.state.add('main', mainState);  
 game.state.start('main');
 
-// Resize game when window resizes
 window.addEventListener("resize", function() {
     game.scale.setGameSize(window.innerWidth, window.innerHeight);
 });
